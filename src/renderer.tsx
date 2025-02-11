7a056f038764d61e1b3114acc8d0116a9e54ff16
Soshi Teshigawara <chanya.vrchat@gmail.com>
2025-02-12 02:15:56 +0900

    Add viewport

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,9 +4,10 @@ export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link href="/static/style.css" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body style={{ overflow: 'hidden' }}>{children}</body>
     </html>
   )
 })
